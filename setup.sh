f132fbc95f245d587e04e7e8a5a27a23eb066cec
Yusuke Nozoe <yusuke.nozoe@gmail.com>
2015-10-02 20:58:05 +0900

    add minimum settings .gitconfig and .vimrc

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOT_FILES=( .gitconfig .vimrc )
+
+for file in ${DOT_FILES[@]}
+do
+    if [ -e $HOME/$file ]; then
+        rm -rdf $HOME/$file
+        echo "$file を削除しました。"
+    fi
+    ln -s $HOME/dotfiles2/$file $HOME/$file
+done
+
+[ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
